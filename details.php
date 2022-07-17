810cc13597aa6743daa51656b21cf2cbaac0c52a
MichaelGarrigosa <mgarrigosa90@gmail.com>
2022-07-17 14:30:31 +0200

    1430

--- details.php
+++ details.php
@@ -1,8 +1,8 @@
 <?php
 require_once "db_connect.php";
 
-$id = $_GET["id"];
-$sql = "select * from user where id = $id";
+$id = $_GET["book_library"];
+$sql = "select * from book_library where book_library = $book_library";
 $result = mysqli_query($conn, $sql);
 
 $row = mysqli_fetch_assoc($result);
@@ -17,17 +17,21 @@ $row = mysqli_fetch_assoc($result);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
 </head>
 
 <body>
     <div class='card' style='width: 18rem;'>
         <div class='card-body'>
-            <h5 class='card-title'><?= "{$row["first_name"]} {$row["last_name"]}"  ?> </h5>
-            <p class='card-text'><?= $row["email"] . " | " . $row["status"] ?></p>
-            <a href='update.php?id=<?= $row["id"] ?>' class='btn btn-warning'>Update</a>
-            <a href='delete.php?id=<?= $row["id"] ?>' class='btn btn-danger'>Delete</a>
+            <h5 class='card-title'><?= "{$row["author_first_name"]} {$row["author_last_name"]}"  ?> </h5>
+            <p class='card-text'><?= $row["book_library"] . " | " . $row["image"] . " | " .$row["isbn"] . " | " . $row["publisher_adress"] . " | " .$row["author_first_name"] . " | " . $row["publish_date"]
+            $row["short_discrition"] . " | " .$row["status"] . " | " . $row["title"] . " | " .$row["type"]?></p>
+            <a href='update.php?id=<?= $row["book_library"] ?>' class='btn btn-warning'>Update</a>
+            <a href='delete.php?id=<?= $row["book_library"] ?>' class='btn btn-danger'>Delete</a>
+
+
         </div>
     </div>
 </body>
