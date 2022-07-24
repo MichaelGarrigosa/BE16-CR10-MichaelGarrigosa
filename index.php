192f71b0d4f472bdc6d103ae532d49f371dbb522
MichaelGarrigosa <mgarrigosa90@gmail.com>
2022-07-24 15:12:51 +0200

    1515

--- index.php
+++ index.php
@@ -12,23 +12,22 @@ if (mysqli_num_rows($result) == 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         $body .= "<div class='card' style='width: 18rem;'>
   <div class='card-body'>
-  <h5 class='card-title'>{$row["image"]} {$row["isbn"]} {$row["publisher_adress"]} {$row["author_first_name"]} {$row["author_last_name"]} 
-  {$row["publish_date"]}{$row["short_discription"]}{$row["status"]}{$row["title"]}{$row["type"]}
-
+  <h5 class='card-title'>{$row["id"]} {$row["title"]} {$row["image"]} {$row["isbn"]} {$row["short_discription"]} {$row["type"]} {$row["author_first_name"]} {$row["author_last_name"]}{$row["publisher_name"]} {$row["publisher_adress"]} {$row["publish_date"]}{$row["status"]} 
   
+  <p class='card-text'>{$row["id"]}</p>;
+  <p class='card-text'>{$row["title"]}</p>;
   <p class='card-text'>{$row["image"]}</p>;
   <p class='card-text'>{$row["isbn"]}</p>;
-  <p class='card-text'>{$row["publisher_adress"]}</p>;
+  <p class='card-text'>{$row["short_discription"]}</p>;
+  <p class='card-text'>{$row["type"]}</p>;
   <p class='card-text'>{$row["author_first_name"]}</p>;
   <p class='card-text'>{$row["author_last_name"]}</p>;
+  <p class='card-text'>{$row["publisher_name"]}</p>;
+  <p class='card-text'>{$row["publisher_adress"]}</p>;
   <p class='card-text'>{$row["publish_date"]}</p>;
-  <p class='card-text'>{$row["short_discription"]}}</p>;
   <p class='card-text'>{$row["status"]}</p>;
-  <p class='card-text'>{$row["title"]}</p>;
-  <p class='card-text'>{$row["type"]}</p>;
-
-   
-    <a href='details.php?id={$row["book_library"]}' class='btn btn-primary'>Details</a>
+  
+ <a href='details.php?id={$row["book_library"]}' class='btn btn-primary'>Details</a>
     <a href='update.php?id={$row["book_library"]}' class='btn btn-warning'>Update</a>
     <a href='delete.php?id={$row["book_library"]}' class='btn btn-danger'>Delete</a>
   </div>
@@ -45,14 +44,16 @@ mysqli_close($conn);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
 </head>
 
 <body>
     <a href="create.php">Create User</a>
     <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+            data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown button
         </button>
         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
@@ -69,7 +70,8 @@ mysqli_close($conn);
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
     </script>
 
 </body>
